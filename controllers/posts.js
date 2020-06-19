855fd137f827ba8410a2b5cb39faba71b10d980d
Ivan Yerkinov <darkblackvoid@yahoo.com>
2020-06-19 11:05:29 -0700

    Users can see induvidual posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -22,6 +22,17 @@ module.exports = (app) => {
   .catch(err => {
     console.log(err.message);
   });
-  })
+});
+
+  app.get("/posts/:id", function(req, res) {
+  // LOOK UP THE POST
+  Post.findById(req.params.id)
+    .then(post => {
+      res.render("posts-show", { post });
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+});
 
 };
