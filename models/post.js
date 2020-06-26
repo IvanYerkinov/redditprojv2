28b7e76876034244a3d9c598b29300e3cdee9b59
Ivan Yerkinov <darkblackvoid@yahoo.com>
2020-06-26 11:27:47 -0700

    refactored populate code

--- models/post.js
+++ models/post.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Populate = require("../util/autopopulate");
+
 
 const PostSchema = new Schema({
   title: { type: String, required: true },
@@ -10,5 +12,8 @@ const PostSchema = new Schema({
   author : { type: Schema.Types.ObjectId, ref: "User", required: true }
 
 });
+PostSchema
+    .pre('findOne', Populate('author'))
+    .pre('find', Populate('author'))
 
 module.exports = mongoose.model("Post", PostSchema);
